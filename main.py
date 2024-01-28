c4cdddc3d8c925838cac397b82f385162a654c1d
avycado13 <108358183+avycado13@users.noreply.github.com>
2024-01-28 11:26:13 -0800

    fix more stuff

--- main.py
+++ main.py
@@ -7,11 +7,39 @@ import configparser
 import os
 import subprocess
 import docker
+from stat import S_IXUSR
 from git import Repo
 import git
+import click
+import sys
+
+# Vars
+PIKU_RAW_SOURCE_URL = "https://raw.githubusercontent.com/piku/piku/master/piku.py"
+PIKU_ROOT = os.environ.get("PIKU_ROOT", os.path.join(os.environ["HOME"], ".piku"))
+PIKU_BIN = os.path.join(os.environ["HOME"], "bin")
+PIKU_SCRIPT = os.path.realpath(__file__)
+PIKU_PLUGIN_ROOT = os.path.abspath(os.path.join(PIKU_ROOT, "plugins"))
+APP_ROOT = os.path.abspath(os.path.join(PIKU_ROOT, "apps"))
+DATA_ROOT = os.path.abspath(os.path.join(PIKU_ROOT, "data"))
+ENV_ROOT = os.path.abspath(os.path.join(PIKU_ROOT, "envs"))
+GIT_ROOT = os.path.abspath(os.path.join(PIKU_ROOT, "repos"))
+ACME_ROOT = os.environ.get("ACME_ROOT", os.path.join(os.environ["HOME"], ".acme.sh"))
+ACME_WWW = os.path.abspath(os.path.join(PIKU_ROOT, "acme"))
+ACME_ROOT_CA = os.environ.get("ACME_ROOT_CA", "letsencrypt.org")
 
 # Set up logging
 logging.basicConfig(filename="deploy.log", level=logging.INFO)
+
+
+def sanitize_app_name(app):
+    """Sanitize the app name and build matching path"""
+
+    app = (
+        "".join(c for c in app if c.isalnum() or c in (".", "_", "-"))
+        .rstrip()
+        .lstrip("/")
+    )
+    return app
 
 
 def nginx_configure(nginx, ip: str, name: str, port: int, ssl: bool) -> None:
@@ -285,26 +313,86 @@ def connect_container_to_network(container, network) -> None:
     network.connect(container)
 
 
-def deploy() -> None:
+@click.command("git-hook")
+@click.argument("app")
+def cmd_git_hook(app):
+    """INTERNAL: Post-receive git hook"""
+
+    app = sanitize_app_name(app)
+    repo_path = os.path.join(GIT_ROOT, app)
+    app_path = os.path.join(APP_ROOT, app)
+    data_path = os.path.join(DATA_ROOT, app)
+
+    for line in sys.stdin:
+        # pylint: disable=unused-variable
+        oldrev, newrev, refname = line.strip().split(" ")
+        # Handle pushes
+        if not os.path.exists(app_path):
+            click.echo("-----> Creating app '{}'".format(app), fg="green")
+            os.makedirs(app_path)
+            # The data directory may already exist, since this may be a full redeployment (we never delete data since it may be expensive to recreate)
+            if not os.path.exists(data_path):
+                os.makedirs(data_path)
+            clone_or_pull_repo(app, repo_path)
+        deploy(app, newrev=newrev)
+
+
+@click.command("git-receive-pack")
+@click.argument("app")
+def cmd_git_receive_pack(app):
+    """INTERNAL: Handle git pushes for an app"""
+
+    app = sanitize_app_name(app)
+    hook_path = os.path.join(GIT_ROOT, app, "hooks", "post-receive")
+    env = globals()
+    env.update(locals())
+
+    if not os.exists(hook_path):
+        os.makedirs(os.dirname(hook_path))
+        # Initialize the repository with a hook to this script
+        subprocess.call("git init --quiet --bare " + app, cwd=GIT_ROOT, shell=True)
+        with open(hook_path, "w") as h:
+            h.write(
+                """#!/usr/bin/env bash
+set -e; set -o pipefail;
+cat | PIKU_ROOT="{PIKU_ROOT:s}" {PIKU_SCRIPT:s} git-hook {app:s}""".format(**env)
+            )
+        # Make the hook executable by our user
+        os.chmod(hook_path, os.stat(hook_path).st_mode | S_IXUSR)
+    # Handle the actual receive. We'll be called with 'git-hook' after it happens
+    subprocess.call(
+        'git-shell -c "{}" '.format(sys.argv[1] + " '{}'".format(app)),
+        cwd=GIT_ROOT,
+        shell=True,
+    )
+
+
+def deploy(app: str, newrev = None) -> None:
     """
     Deploy the application.
     """
     try:
+
+        # Clone the repository if it doesn't exist, or fetch the latest changes
+        repo_path = os.path.join(APP_ROOT, app)
+        env = {'GIT_WORK_DIR': repo_path}
+        click.echo("-----> Deploying app '{}'".format(app), fg='green')
+        subprocess.call('git fetch --quiet', cwd=repo_path, env=env, shell=True)
+        
+        if newrev:
+            subprocess.call('git reset --hard {}'.format(newrev), cwd=repo_path, env=env, shell=True)
+        subprocess.call('git submodule init', cwd=repo_path, env=env, shell=True)
+        subprocess.call('git submodule update', cwd=repo_path, env=env, shell=True)
         # Check if the config file exists
-        config_path = "pyoku.ini"
+        config_path = os.path.join(repo_path,"pyoku.ini")
         config = load_config(config_path)
         if not config:
             return
-
-        # Clone the repository if it doesn't exist, or fetch the latest changes
-        repo_path = "/" + get_repo_name()
-        repo_url = get_repo_url()
-        clone_or_pull_repo(repo_path, repo_url)
-
+        
         # Check if the Docker daemon is running
         if not check_docker_daemon():
             return
-
+        
         client = docker.from_env()
         # Build a Docker image from the Dockerfile in the repo
         image = build_docker_image(repo_path, client)
@@ -337,22 +425,12 @@ def deploy() -> None:
         )
 
         # Log successful deployment
-        logging.info("Deployment successful")
+        click.echo("Deployment successful")
 
     except git.exc.GitCommandError as e:
-        logging.error("Git command error: %s", e)
+        click.echo("Git command error: %s", e)
     except docker.errors.APIError as e:
-        logging.error("Docker API error: %s", e)
+        click.echo("Docker API error: %s", e)
     except Exception as e:
-        logging.error("Deployment failed: %s", e)
+        click.echo("Deployment failed: %s", e)
 
-
-def main() -> None:
-    """
-    CLI
-    """
-    deploy()
-
-
-if __name__ == "__main__":
-    main()
